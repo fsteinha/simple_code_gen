3d6664586e63072412c9dafb39d3975362897c0d
fsteinha <fsteinha@web.de>
2015-01-07 09:56:12 +0100

    attreader Filename test exex name

--- lib/scg_unit_test.rb
+++ lib/scg_unit_test.rb
@@ -2,11 +2,17 @@ $: << File.expand_path( "." )
 
 require "scg_unit"
 require "scg_module"
+require "scg_build"
 
 class UnitTest
+  attr_reader:test_file_name
+  attr_reader:unit_test_exe_name
+  
   def initialize( unit, scg_module, test_file_name = nil, 
                     test_file_name_extension = nil, 
-                    editor_col_count=79, unit_test_suffix = "unit_test" )
+                    editor_col_count=79, unit_test_suffix = "unit_test",
+                    code_gen = nil, code_pars = nil,
+                    build = nil, unit_test_exe_name = nil)
                     
     raise "Error: Wrong class" if !unit.kind_of?( Unit )
     raise "Error: Wrong value" if unit == nil
@@ -15,12 +21,20 @@ class UnitTest
     
     @unit = unit
     @module = scg_module
+    
     if test_file_name != nil
       @test_file_name = test_file_name 
     else
       @test_file_name = "unit_test_" + unit.name
       @test_file_name = @test_file_name + test_file_name_extension  if test_file_name_extension != nil
     end
+    
+    if unit_test_exe_name != nil
+      @unit_test_exe_name = unit_test_exe_name 
+    else
+      @unit_test_exe_name = "unit_test_" + unit.name
+    end
+          
     @editor_col_count = editor_col_count
     @code_comment_line_sign = '#'
     @test_file = nil
@@ -31,22 +45,34 @@ class UnitTest
     
     @align_offset = 0
     @align_char = ' '
+    
+    @code_gen = code_gen
+    @code_pars = code_pars
+    
+    @a_tests = []
+      
+    @build = build  
   end
   
   def make
+    @a_tests = []
     @test_file = File.open(@test_file_name, "wb")
     
+    write_global_port_instances()
     write_include_module()
+    
     @test_file.puts
     
+    _write( @code_gen.comment_block(["tests of unit"], @editor_col_count) )
+
     if (@unit.a_global_input.length > 0)
       @unit.a_global_input.each do | input |
         input.a_value_unvalid.each do | value |
           input.set_value( value )
           make_test()
         end
-      
-        input.a_value_valid.each do | value |
+              
+        input.a_value_valid.each do | value |  
           input.set_value( value )
           make_test_loc()
         end      
@@ -55,7 +81,11 @@ class UnitTest
       make_test_loc()
     end
     
+    make_test_exec()
+    
     @test_file.close
+    
+    @build.build(self) if @build != nil
   end
   
   def make_test_loc
@@ -75,6 +105,7 @@ class UnitTest
   def make_test
 
     test_name = @unit_test_suffix.clone
+    test_name << "__#{@a_tests.length}__"
 
     @unit.a_global_input.each do | input |
       if input.kind_of?( Port_num )
@@ -96,7 +127,8 @@ class UnitTest
       end
     end    
 
-    puts test_name
+    @a_tests << test_name
+#    puts test_name
 
     # write the test
     write_open_unit_test(test_name)
@@ -141,27 +173,48 @@ class UnitTest
   end
   
   # virtual must be written by successor
+  def make_test_exec
+    raise ( "Error: virtual method!!")    
+  end
+      
   def write_close_unit_test()
     @align_offset = 0
     _puts
+    _puts( @code_gen._return(@code_gen.const_true))
     _puts( @code_gen.block_close)
   end
+  
+  def write_global_port_instances
+    _write( @code_gen.comment_block(["global inputs"], @editor_col_count) )
+    @unit.a_global_input.each do | port |
+      _puts( @code_gen.set_port_declaration(port) )
+    end
 
+    _write( @code_gen.comment_block(["global outputs"], @editor_col_count) )
+    @unit.a_global_output.each do | output |
+      _puts( @code_gen.set_port_declaration(output.port) )
+    end
+  end
+  
   def write_include_module
-    _puts @code_gen.include( @module.name )
+    #_puts @code_gen.include( @module.name )
+    raise ("Error: NO code parser !!") if @code_pars == nil      
+    _write( @code_gen.comment_block(["unit to be test", "extract from #{@module.name}"], @editor_col_count) )
+    _write( @code_pars.get_unit( @module.name,@unit.name ) )
+    _puts
   end
 
   def write_output_check
     @unit.a_local_output.each do | output |
       param = output.a_rule_param
       param = output.a_rule_param[0] if output.a_rule_param.length <= 1
-      _write ( @code_gen.comparision(output.name, output.rule.call(param), nil, "return 0;", @align_offset) )
+      _write ( @code_gen.comparision(output.name, output.rule.call(param), nil, @code_gen._return(@code_gen.const_false), @align_offset) )
     end
     
     @unit.a_global_output.each do | output |
       param = output.a_rule_param
       param = output.a_rule_param[0] if output.a_rule_param.length <= 1
-      _write ( @code_gen.comparision(output.name, output.rule.call(param), nil, "return 0;", @align_offset) )
+      _write ( @code_gen.comparision(output.name, output.rule.call(param), nil, @code_gen._return(@code_gen.const_false), @align_offset) )
     end
     
   end
