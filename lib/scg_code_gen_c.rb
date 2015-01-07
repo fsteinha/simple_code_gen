aa99cd15b27c3db53f802af9dc6f69b74248716d
fsteinha <fsteinha@web.de>
2015-01-07 09:50:48 +0100

    comment functions, renames

--- lib/scg_code_gen_c.rb
+++ lib/scg_code_gen_c.rb
@@ -9,7 +9,7 @@ class CodeGenC < CodeGen
     super
   end
   
-  def declaration ( port )
+  def set_port_declaration ( port )
     raise ( "Error: Wrong class" ) if !port.kind_of?(Port)
   
     case port.class
@@ -51,9 +51,27 @@ class CodeGenC < CodeGen
     return ret
   end
 
-  #virtual
   def include (modul)
-    _puts( "#include \"#{modul}\"" )
+    return _puts( "#include \"#{modul}\"" )
+  end
+
+  def comment(comment)
+    return _write( "/* #{coment} */" )
+  end
+
+  def comment_block(a_comment, page_width)
+    ret = "/*"
+    ret << _puts
+    ret <<  " *" 
+    3.upto(page_width).each { ret << _write( '*' ) }
+    ret << _puts
+    a_comment.each {|comment| ret << " * #{comment}\n" }
+    ret <<  " *" 
+      3.upto(page_width).each { ret <<  '*' }
+    ret << _puts
+    ret <<  " */" 
+    ret << _puts
+    return ret
   end
 
   def block_open ( )
@@ -66,6 +84,22 @@ class CodeGenC < CodeGen
     align_minus
     ret = _puts( "}" )
     return ret
+  end
+
+  def _return(value)
+    return _puts("return #{value};")
+  end
+
+  def set_constant(constant_name, value)
+    return _puts("#define #{constant_name} #{value}")
+  end
+
+  def const_true()
+    return "1"
+  end
+
+  def const_false()
+    return "0"
   end
 
 end
